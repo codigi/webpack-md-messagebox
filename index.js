5f61d74fbd4423cfd6461c6abd725386d97b5884
Atanas Palavrov <palavrov@gmail.com>
2017-05-18 22:05:21 +0200

    Close message box on enter.

--- index.js
+++ index.js
@@ -2,7 +2,17 @@ var $mdDialog = require("webpack-mddialog-bootstrap");
 
 module.exports = function(title, message) {
     $mdDialog.show($mdDialog.alert({
-        template: '<md-dialog><md-toolbar><div class="md-toolbar-tools"><h2>'+title+'</h2></div></md-toolbar><md-dialog-content layout-padding>'+message+'</md-dialog-content><md-dialog-actions layout-padding><md-button class="md-raised md-primary md-hue-3" blur-on-focus ng-click="dialog.hide()">OK</md-button></md-dialog-actions></md-dialog>',
+        template:
+                '<md-dialog>'+
+                    '<form ng-submit="dialog.hide()">'+
+                        '<md-toolbar><div class="md-toolbar-tools"><h2>'+title+'</h2></div></md-toolbar>'+
+                        '<md-dialog-content layout-padding>'+message+'</md-dialog-content>'+
+                        '<md-dialog-actions layout-padding>'+
+                            '<input style="position: absolute; top: -9999px; left: -9999px;" tabindex="-1" autofocus type="text">'+
+                            '<md-button type="submit" class="md-raised md-primary md-hue-3" blur-on-focus ng-click="dialog.hide()">OK</md-button>'+
+                        '</md-dialog-actions>'+
+                    '</form>'+
+                '</md-dialog>',
         skipHide: true,
         escapeToClose: true,
         clickOutsideToClose: false,
